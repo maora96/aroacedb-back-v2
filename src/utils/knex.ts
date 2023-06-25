6389d58cf9efc1b9651083b9392958fff46eba45
maora <marinaoliveira.r07@gmail.com>
2023-06-24 21:49:06 -0300

    fix: database connection

--- src/utils/knex.ts
+++ src/utils/knex.ts
@@ -2,17 +2,18 @@ import knexLib from 'knex';
 
 export const knex = knexLib({
   client: 'pg',
-  connection: {
-    // host: 'localhost',
-    // port: 5432,
-    // user: 'postgres',
-    // database: 'aroacedb',
-    // password: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    ssl: true,
-  },
+  connection: process.env.DATABASE_URL,
+  // connection: {
+  //   // host: 'localhost',
+  //   // port: 5432,
+  //   // user: 'postgres',
+  //   // database: 'aroacedb',
+  //   // password: 'postgres',
+  //   host: process.env.POSTGRES_HOST,
+  //   port: Number(process.env.POSTGRES_PORT),
+  //   user: process.env.POSTGRES_USER,
+  //   password: process.env.POSTGRES_PASSWORD,
+  //   database: process.env.POSTGRES_DB,
+  //   ssl: true,
+  // },
 });
